8f742574e1968ad29e86056781a1fb9d26b8928d
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-06-14 23:40:44 +0300

    Exporting files to run the application

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='1234'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:kiplangat18@localhost/pitches'
+export MAIL_USERNAME='kiplangatdennis99@gmail.com'
+export MAIL_PASSWORD='kiplangat18'
+
+python3.8 manage.py server
